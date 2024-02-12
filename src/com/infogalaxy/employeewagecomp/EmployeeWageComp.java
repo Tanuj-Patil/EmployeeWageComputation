e6bad7bc225b0097d5e454c1898512acf40f3c21
TanujPatil <tanujpatil9561@gmail.com>
2024-02-12 19:03:54 +0530

    [Added]: by Tanuj - Employee Wage Computation Project Setup

---
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -0,0 +1,16 @@
+package com.infogalaxy.employeewagecomp;
+
+import java.util.Scanner;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+
+		int attendance = 0;
+		
+		System.out.println("Enter [0 -Absent] / [1 -present]");
+		
+		
+	}
+
+}
