2d6b6c12981b6d60224ef1481434abb1a0e3bfa9
TanujPatil <tanujpatil9561@gmail.com>
2024-02-12 19:11:33 +0530

    [MODIFIED]: by Tanuj - UC-1-Completed for Employee Attendance

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		int attendance = 0;
 		
 		System.out.println("Enter [0 -Absent] / [1 -present]");
 		
+		attendance = sc.nextInt();
+		
+		if(attendance == 0) {
+			System.out.println("Absent");
+		} else {
+			System.out.println("Present");
+		}
 		
 	}
 
