2449c6844eb47493b4e56842ca764b9f46e887a9
TanujPatil <tanujpatil9561@gmail.com>
2024-02-13 14:58:25 +0530

    [MODIFIED]: by Tanuj - Java Code to calculate Daily Wage of Employee

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -1,21 +1,32 @@
 package com.infogalaxy.employeewagecomp;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		
 		int attendance = 0;
+		int dailyWage =  0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		
-		System.out.println("Enter [0 -Absent] / [1 -present]");
+//		Using Random class 
+		Random random = new Random();
 		
-		attendance = sc.nextInt();
+		attendance = random.nextInt(9)% 2;  // inside nextInt() we can provide Upper Bound value and %2 for 0 or 1 as result.
+
+//		Using random() method of Math class which is static
+//		attendance = (int) Math.floor(Math.random());
+//		System.out.println(attendance);
+		
+		dailyWage = WAGE_PER_HR * FULL_DAY_HR;
 		
 		if(attendance == 0) {
 			System.out.println("Absent");
 		} else {
 			System.out.println("Present");
+			System.out.println("The total Wage of a worker is :" +dailyWage);
 		}
 		
 	}
