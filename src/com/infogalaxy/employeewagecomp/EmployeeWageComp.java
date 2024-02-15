c761792acb9c315e2f80cdffcb9b3491efdd7515
TanujPatil <tanujpatil9561@gmail.com>
2024-02-15 20:51:46 +0530

    [MODIFIED]: by Tanuj - Calculated Employee Wage per Month based on Attendance and Work Type

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -7,34 +7,38 @@ public class EmployeeWageComp {
 	        Random random = new Random();
 
 	        int attendance;
-	        int dailyWage;
 	        int employee_type;
+	        int dailyWage = 0;
 	        final int PART_TIME_HR = 4;
 	        final int FULL_TIME_HR = 8;
 	        final int WAGE_PER_HR = 20;
+	        final int MAX_WORKING_DAY_PER_MONTH = 20;
 	        
-	        attendance = random.nextInt(9) % 2;
-
-	        switch (attendance) {
-	            case 0:
-	                System.out.println("Absent");
-	                break;
-	            
-	            case 1:
-	                System.out.println("Present");
-	                employee_type = random.nextInt(9) % 2;
-	                
-	                if (employee_type == 0) {
-	                    dailyWage = WAGE_PER_HR * PART_TIME_HR;
-	                } else {
-	                    dailyWage = WAGE_PER_HR * FULL_TIME_HR;
-	                }
-	                System.out.println("The wage of Employee is: " +dailyWage);
-	                break;
-
-	            default:
-	                System.out.println("Enter correct data!");
-	                break;
-	        }
+	        for(int i = 1; i <= MAX_WORKING_DAY_PER_MONTH; i++) {	        
+		        attendance = random.nextInt(9) % 2;
+		        switch (attendance) {
+		            case 0:
+		                System.out.println("Absent");
+		                break;
+		            
+		            case 1:
+		                System.out.println("Present");
+		                
+		                employee_type = random.nextInt(9) % 2;
+			                
+			            if (employee_type == 0) {
+			                dailyWage = dailyWage + (WAGE_PER_HR * PART_TIME_HR);
+			            } else {
+			                dailyWage = dailyWage + (WAGE_PER_HR * FULL_TIME_HR);
+			            }
+//			            System.out.println("The wage of Employee is: " +dailyWage);
+		                break;
+	
+		            default:
+		                System.out.println("Enter correct data!");
+		                break;
+		        }
+	        }    
+	        System.out.println("The wage of Employee is: " +dailyWage);
 	    }
 }
