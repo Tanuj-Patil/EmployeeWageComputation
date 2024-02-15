712f0fc634f32e1ddbd13caf60066587ec82561c
TanujPatil <tanujpatil9561@gmail.com>
2024-02-15 09:59:15 +0530

    [MODIFIED]: by Tanuj - Java code to calculate Employee wage based on Work type (Part/Full)

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -6,10 +6,12 @@ public class EmployeeWageComp {
 
 	public static void main(String[] args) {
 		
-		int attendance = 0;
-		int dailyWage =  0;
+		int attendance;
+		int dailyWage;
+		int employee_type;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
+		final int PART_TIME_HR = 4;
+		final int FULL_TIME_HR = 8;
 		
 //		Using Random class 
 		Random random = new Random();
@@ -20,12 +22,16 @@ public class EmployeeWageComp {
 //		attendance = (int) Math.floor(Math.random());
 //		System.out.println(attendance);
 		
-		dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-		
 		if(attendance == 0) {
 			System.out.println("Absent");
 		} else {
 			System.out.println("Present");
+			employee_type = random.nextInt(9)% 2;
+			if(employee_type == 0) {
+				dailyWage = WAGE_PER_HR * PART_TIME_HR;
+			} else {
+				dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+			}
 			System.out.println("The total Wage of a worker is :" +dailyWage);
 		}
 		
